b7d567e7eb342ce0e1a692f08ec73ed3873e1e82
Jiyeon-rhd <yjyshsj0217@gamil.com>
2024-09-09 12:10:56 +0900

    프론트 업로드

---
+++ src/main/java/com/example/hue/controller/homecontroller.java
@@ -0,0 +1,15 @@
+package com.example.hue.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class homecontroller {
+
+    @GetMapping("/detail")
+    public String detail() {
+        return "detail";
+    }
+
+
+}
